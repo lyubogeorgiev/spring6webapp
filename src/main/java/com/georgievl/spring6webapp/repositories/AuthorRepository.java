d5a381715cedfa942160cd8508e11e71cf2ff3a0
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-07-04 11:05:48 -0600

    Author Repository added

---
+++ src/main/java/com/georgievl/spring6webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.georgievl.spring6webapp.repositories;
+
+import com.georgievl.spring6webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
